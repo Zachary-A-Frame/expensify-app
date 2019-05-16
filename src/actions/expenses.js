2d0000ac4e10c4b38827197ba38fde1f77698b08
framezach <framez@bgsu.edu>
2019-05-16 16:43:45 -0400

    Setup test database environment.

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,24 +1,46 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase.js'
+
+// ------ Outline: 
+
+// Components call action generator
+// Action generator returns object
+// Component dispatches object
+// Redux store changes
+
+// ----- TODO: Asynchronous Actions 
+
+// Components calls action generator 
+// Action generator returns function
+// Component dispatches function (?)
+// Function runs (has the ability to dispatch to other actions and do whatever it wants)
+
+
 
 // ADD_EXPENSE
-export const addExpense = (
-  {
-    description = '',
+export const addExpense = (expense) => ({
+  type: 'ADD_EXPENSE',
+  expense
+})
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '',
     note = '',
     amount = 0,
-    createdAt = 0,
-    expenseCount = 0
-  } = {}
-) => ({
-  type: 'ADD_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
+    createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt }
+
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }))
+    })
   }
-});
+}
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
---
+++ src/firebase/firebase.js
@@ -0,0 +1,59 @@
+import * as firebase from 'firebase'
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config)
+
+const database = firebase.database()
+
+export { firebase, database as default }
+
+// // Subscription to expenses, array now updates on database change. 
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             // Requires id
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//             // Creating a new array by iterating through all snapshots
+//         })
+//     })
+//         console.log(expenses)
+// })
+
+// // child_removed subscriber
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// })
+
+// // child_changed subscriber
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+
+// // child_cadded subscriber - will print two arrays, check documentation for more information. 
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+
+
+// // This is the same thing as using. .set(null) = remove
+// // database.ref('isSingle').set(null)
+
+// // database.ref()
+// //     .remove()
+// //     .then(() => {
+// //         console.log("Remove succeeded.")
+// //   })
+// //     .catch((e) => {
+// //         console.log("Remove failed: " + error.message)
+// //   });
